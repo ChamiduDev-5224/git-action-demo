1f6a8e9f36ac0c0ae100310df49953a9a13fd958
Rachitha95 <rachitharangana9522@gmail.com>
2024-05-19 22:52:16 +0530

    content update

--- resources/views/pages/dashboard/dashboardCreate.blade.php
+++ resources/views/pages/dashboard/dashboardCreate.blade.php
@@ -7,6 +7,6 @@
     <title>Git-action demo project</title>
 </head>
 <body>
-    
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis molestias praesentium, recusandae corrupti laboriosam illum enim in culpa perferendis nobis atque at, quas, cupiditate odit ab assumenda quod excepturi qui.</p>
 </body>
 </html>
